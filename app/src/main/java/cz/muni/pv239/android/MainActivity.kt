9dad774e9468385d9a2e2e700bfa417563e6a267
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2020-03-17 22:19:19 +0100

    Implemented authentication
    * Added facebookSdk for authentication.
    
    * The authentication is working but not all use cases are handled for
    now.

--- app/src/main/java/cz/muni/pv239/android/MainActivity.kt
+++ app/src/main/java/cz/muni/pv239/android/MainActivity.kt
@@ -1,12 +1,58 @@
 package cz.muni.pv239.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.ToolTipPopup
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private var callbackManager = CallbackManager.Factory.create()
+
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        login_button.setToolTipStyle(ToolTipPopup.Style.BLACK)
+
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+
+        if (isLoggedIn) {
+            onLoginSuccess()
+        }
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    onLoginSuccess()
+                }
+
+                override fun onCancel() {
+                }
+
+                override fun onError(error: FacebookException?) {
+                }
+        })
+    }
+
+    fun onLoginSuccess() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
