4f54bddfbcf618bdcc818dd1c6cc13b3f46c6264
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2020-03-18 18:16:27 +0100

    Added material design
    * Added material design library and edited existing components.

--- app/src/main/java/cz/muni/pv239/android/ui/fragments/NickNameFragment.kt
+++ app/src/main/java/cz/muni/pv239/android/ui/fragments/NickNameFragment.kt
@@ -1,12 +1,16 @@
 package cz.muni.pv239.android.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
 import cz.muni.pv239.android.R
+import kotlinx.android.synthetic.main.fragment_nick_name.*
 
 class NickNameFragment : Fragment() {
 
@@ -19,7 +23,22 @@ class NickNameFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nick_name, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_nick_name, container, false)
+
+        val editText = view.findViewById<EditText>(R.id.nick_name_edit);
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                confirm_button.isEnabled = s.toString().trim().isNotEmpty()
+            }
+        })
+
+        return view
     }
 }
